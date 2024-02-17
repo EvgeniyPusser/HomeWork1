d9f1184212757717793c7df81d14c052c60de939
Evgeniy Pusser <evgeniipusser88@gmail.com>
2024-02-17 22:59:57 +0300

    Second Task

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] myArr =  threeDigNumbers(10, 100, 1000);
+int EvenNumbersAmount = EvenNumbers(myArr);
+Console.Write(EvenNumbersAmount + "\n");
+PrintArray(myArr);
+
+
+
+ int[] threeDigNumbers(int size, int LeftBoard, int RightBoard)
+{
+  
+   int[] array = new int[size];
+   
+   for(int i = 0; i < size; i++){
+    array[i] = new Random().Next(LeftBoard, RightBoard + 1);
+   }
+   return array;
+}
+
+int EvenNumbers(int[] array){
+int count = 0;
+for(int i = 0; i < array.Length; i++){
+    if(array[i]%2 == 0)
+    count++;
+   }
+   return count;
+
+}
+
+void PrintArray(int[] array){
+
+   for(int i = 0; i < array.Length; i++){
+      Console.Write(array[i] + "\t");
+   }
+}
